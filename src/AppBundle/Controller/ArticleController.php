2eb4f74d29bce52f44260f9d807f379f2d6e911f
Михаил Абросимов <michael.abrosimov@gillsoft.tech>
2017-04-22 13:58:46 +0300

    Реализовал Index https://www.evernote.com/l/AiXnFIXwvadI16WWQyCSxP1cHNSC8mifQQAB/image.png

--- src/AppBundle/Controller/ArticleController.php
+++ src/AppBundle/Controller/ArticleController.php
@@ -1,6 +1,7 @@
 <?php
 namespace AppBundle\Controller;
 
+use AppBundle\Entity\Article;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
@@ -11,12 +12,15 @@ class ArticleController extends Controller
 {
 
     /**
-     * @Route("/article/controller")
+     * @Route("/article/controller", name="article_index")
      */
     public function createGreed()
     {
+        $article = $this->getDoctrine()->getRepository(Article::class);
+        $article = $article->findAll();
+        var_dump($article);
         return $this->render ( 'article/articleGreed.html.twig' , array (
-            'name' => "Article"));
+            'article' => $article));
+        return;
     }
-
 }
